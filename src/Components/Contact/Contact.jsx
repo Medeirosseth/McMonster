ed8a115ec1b201fdd0572e3ab3592e1a3c9f4582
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-23 20:32:19 -0700

    WIP commented out contact to compile previous changes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./Contact.scss";
+// import React, { useState } from "react";
+// // import axios from "axios";
+// import "./Contact.scss";
 
-export default function Contact() {
-  const [message, setMessage] = useState(false);
+// // export default function Contact() {
+// //   const [message, setMessage] = useState(false);
 
-  const formId = "GGGz9wQR";
-  const formSparkUrl = `https://submit-form.com/${formId}`;
+// //   const formId = "GGGz9wQR";
+//  //const formSparkUrl = `https://submit-form.com/${formId}`;
 
-  const submitForm = async (event) => {
-    event.preventDefault();
-    await postSubmission();
-  };
+//   // const submitForm = async (event) => {
+//   //   event.preventDefault();
+//   //   await postSubmission();
+//   // };
 
-  const postSubmission = async () => {
-    const payLoad = {
-      message: "Test formspark submission",
-    };
+//   // const postSubmission = async () => {
+//   //   const payLoad = {
+//   //     message: "Test formspark submission",
+//   //   };
 
-    try {
-      const result = await axios.postSubmission(formSparkUrl, payLoad);
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   //   try {
+//   //     const result = await axios.postSubmission(formSparkUrl, payLoad);
+//   //     console.log(result);
+//   //   } catch (error) {
+//   //     console.log(error);
+//   //   }
+//   // };
 
-  return (
-    <div className="contact" id="contact">
-      <div className="left">
-        <img
-          src="assets/SpatialAwareness.jpg"
-          alt="Two ghost walking along the river with a deer"
-        />
-      </div>
-      <div className="right">
-        <h2>Contact</h2>
-        <form onSubmit={submitForm}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">SEND</button>
-          {message && <span>thx :D</span>}
-        </form>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="contact" id="contact">
+//       <div className="left">
+//         <img
+//           src="assets/SpatialAwareness.jpg"
+//           alt="Two ghost walking along the river with a deer"
+//         />
+//       </div>
+//       <div className="right">
+//         <h2>Contact</h2>
+//         <form onSubmit={submitForm}>
+//           <input type="text" placeholder="Email" />
+//           <textarea placeholder="Message"></textarea>
+//           <button type="submit">SEND</button>
+//           {message && <span>thx :D</span>}
+//         </form>
+//       </div>
+//     </div>
+//   );
+// //}

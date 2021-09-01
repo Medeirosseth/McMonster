75275c5781cec36f78a5e29dfddd478b8cbc6392
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-31 21:57:19 -0700

    add product and products jsx

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import Grid from '@material-ui/core'
+import Product from './Product/Product'
+
+const products = [
+  { id:1, name: 'pins', description: 'wizard and moon', price: '$100' },
+  { id:2, name: 'sticker', description: 'looming men ', price: '$150' }
+]
 
 const Products = () => {
-
+  <main>
+    <Grid container justify="center" spacing={4}>
+      {products.map((product) =>(
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Product 
+            product={product}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  </main>
 
 }
 
